e60f13c76a0a4793cd088c37aff5d55e400a930e
NhanNguyen2003 <148311857+NhanNguyen2003@users.noreply.github.com>
2024-01-22 22:50:12 +0700

    Initial commit

---
+++ EbookShop/EbookShopWebParent/EbookShopFrontEnd/src/main/java/com/ebookshop/site/EbookShopFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.ebookshop.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookShopFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookShopFrontEndApplication.class, args);
+	}
+
+}
---
+++ EbookShop/EbookShopWebParent/src/main/java/com/ebookshop/EbookShopWebParentApplication.java
@@ -0,0 +1,13 @@
+package com.ebookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookShopWebParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookShopWebParentApplication.class, args);
+	}
+
+}
