17f60ed36edd23999ac7237669a978e16f94d85c
Ewad245 <89444542+Ewad245@users.noreply.github.com>
2024-03-07 21:34:37 +0700

    Update

--- EbookShop/EbookShopCommon/src/main/java/com/ebookshop/common/entity/Customer.java
+++ EbookShop/EbookShopCommon/src/main/java/com/ebookshop/common/entity/Customer.java
@@ -1,10 +1,14 @@
 package com.ebookshop.common.entity;
 
+import java.sql.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -30,7 +34,30 @@ public class Customer {
 	@Column(name = "phone_number", nullable = false, length = 15)
 	private String phoneNumber;
 	
+	@Column(nullable = false, length = 64)
+	private String addressLine1;
 	
+	@Column(name = "address_line_2",length = 64)
+	private String addressLine2;
 	
+	@Column(nullable = false, length = 45)
+	private String city;
 	
+	@Column(nullable = false,length = 45)
+	private String state;
+	
+	@Column(name = "postal_code", nullable = false,length = 10)
+	private String postalCode;
+	
+	@Column(name = "verification_code", length = 64)
+	private String verificationCode;
+	
+	private boolean enable;
+	
+	@Column(name = "created_time")
+	private Date createdTime;
+	
+	@ManyToOne
+	@JoinColumn(name = "country_id")
+	private Country country;
 }
